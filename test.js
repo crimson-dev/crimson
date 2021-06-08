4ef4baca137227f8a9877e2834d41492fc4bf216
Charan Vasu <49494773+charan1973@users.noreply.github.com>
2021-06-08 23:46:58 +0530

    ci-pipeline: secret check test 2

--- test.js
+++ test.js
@@ -1,2 +1,3 @@
 var DB_URL='mongodb+srv://someonemaybe:ABCDEFGHIJ@cluster0.y3x5a.mongodb.net/theirproject'
+var GOOGLE_API='000000328242-tjpd38oe3aevkrj3ptfuq9ft8tfc04us.apps.googleusercontent.com'
 console.log(DB_URL)
